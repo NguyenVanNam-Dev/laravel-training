5ac9077b39027361367bdfc7b64746c4bee12b88
nam.nguyen11 <nam.nguyen11@ntq-solution.com.vn>
2024-03-21 23:22:52 +0700

    demo Eloquen one-to-one

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,17 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Customer;
+use App\Models\CustomerIdentityNumber;
 
 class TestController extends Controller
 {
     public function index()
     {
-        /**
-         * @var $customer Customer
-         */
-        $customer = Customer::find(1);
-        $identityNumber = $customer->customerIdentityNumber;
-        dd($identityNumber);
-        return 123;
+
+        // retrieve "Identity Number" of customer using Eloquent's dynamic properties.
+//        /**
+//         * @var $customer Customer
+//         */
+//        $customer = Customer::find(1);
+//        $identityNumber = $customer->customerIdentityNumber;
+        // end
+
+        // Get the customer that owns the Identity Number.
+        $identityNumberObj = CustomerIdentityNumber::find(1);
+        dd($identityNumberObj->customer);
+
+
     }
 }
--- app/Models/CustomerIdentityNumber.php
+++ app/Models/CustomerIdentityNumber.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CustomerIdentityNumber extends Model
 {
     use HasFactory;
+
+    /**
+     * Get customer own the Customer Identity Number
+     *
+     * @return BelongsTo
+     */
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
